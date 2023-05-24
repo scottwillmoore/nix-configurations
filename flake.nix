aaea782d48c00c825f5ce832b6e39433e6dd61ba
Scott Moore <me@scottwillmoore.au>
2023-05-24 20:22:05 +1000

    Setup Git and GnuPG

--- flake.nix
+++ flake.nix
@@ -2,28 +2,20 @@
   description = "Scott's Nix configuration";
 
   inputs = {
-    # Home
     home-manager.url = "github:nix-community/home-manager";
     home-packages.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     home-manager.inputs.nixpkgs.follows = "home-packages";
-
-    # NixOS
-    nixos-packages.url = "github:nixos/nixpkgs/nixos-22.11";
-    nixos-wsl.url = "github:nix-community/nixos-wsl";
-
-    nixos-wsl.inputs.nixpkgs.follows = "nixos-packages";
   };
 
   outputs = inputs @ {
     home-manager,
     home-packages,
-    nixos-packages,
-    nixos-wsl,
     ...
   }: let
-    # Home
     mkHomeConfiguration = {
+      emailAddress,
+      fullName,
       homeModule,
       hostPlatform,
       userName,
@@ -36,6 +28,8 @@
           inherit inputs;
           inherit packages;
           settings = {
+            inherit emailAddress;
+            inherit fullName;
             inherit hostPlatform;
             inherit userName;
           };
@@ -43,46 +37,13 @@
         modules = [homeModule];
         pkgs = packages;
       };
-
-    # NixOS
-    mkNixosConfiguration = {
-      hostName,
-      hostPlatform,
-      nixosModule,
-      userName,
-    }: let
-      nixosConfiguration = nixos-packages.lib.nixosSystem;
-      packages = nixos-packages.legacyPackages.${hostPlatform};
-    in
-      nixosConfiguration {
-        modules = [nixosModule];
-        pkgs = packages;
-        specialArgs = {
-          inherit inputs;
-          inherit packages;
-          settings = {
-            inherit hostName;
-            inherit hostPlatform;
-            inherit userName;
-          };
-        };
-      };
   in {
-    # Home
     homeConfigurations = {
       "scott@scott-desktop" = mkHomeConfiguration {
+        emailAddress = "me@scottwillmoore.au";
+        fullName = "Scott Moore";
         homeModule = ./home.nix;
         hostPlatform = "x86_64-linux";
-        userName = "scott";
-      };
-    };
-
-    # NixOS
-    nixosConfigurations = {
-      "scott-desktop" = mkNixosConfiguration {
-        hostName = "scott-desktop";
-        hostPlatform = "x86_64-linux";
-        nixosModule = ./configuration.nix;
         userName = "scott";
       };
     };
--- home.nix
+++ home.nix
@@ -1,5 +1,4 @@
 {
-  lib,
   packages,
   settings,
   ...
@@ -10,7 +9,7 @@
       alejandra
     ];
     stateVersion = "22.11";
-    username = "${settings.userName}";
+    username = settings.userName;
   };
 
   programs = {
@@ -18,204 +17,39 @@
 
     dircolors.enable = true;
 
-    direnv = {
-      enable = true;
-
-      nix-direnv.enable = true;
-    };
-
     git = {
       enable = true;
 
-      userEmail = "scott.will.moore@gmail.com";
-      userName = "Scott Moore";
+      signing = {
+        key = "D6FF1BE917C2FCDF";
+        signByDefault = true;
+      };
+      userEmail = settings.emailAddress;
+      userName = settings.fullName;
     };
+
+    gpg.enable = true;
 
     home-manager.enable = true;
 
-    neovim = {
+    man.enable = true;
+
+    ssh.enable = true;
+
+    starship.enable = true;
+  };
+
+  services = {
+    gpg-agent = {
       enable = true;
 
-      plugins = with packages.vimPlugins; [
-        vim-nix
-      ];
-      vimAlias = true;
-    };
-
-    readline = {
-      enable = true;
-
-      extraConfig = ''
-        set show-mode-in-prompt on
-        set vi-cmd-mode-string "\1\e[2 q\2"
-        set vi-ins-mode-string "\1\e[6 q\2"
-
-        set keymap vi-command
-        "\C-a": beginning-of-line
-        "\C-e": end-of-line
-
-        set keymap vi-insert
-        "\C-a": beginning-of-line
-        "\C-e": end-of-line
-        "\C-l": clear-screen
-        "\C-n": next-history
-        "\C-p": previous-history
-      '';
-      includeSystemConfig = false;
-      variables = {
-        bell-style = "none";
-        colored-stats = true;
-        completion-ignore-case = true;
-        completion-map-case = true;
-        completion-prefix-display-length = 4;
-        completion-query-items = 64;
-        echo-control-characters = false;
-        editing-mode = "vi";
-        keyseq-timeout = 20;
-        mark-symlinked-directories = true;
-        show-all-if-ambiguous = true;
-        show-all-if-unmodified = true;
-      };
-    };
-
-    starship = {
-      enable = true;
-
-      settings = {
-        add_newline = false;
-        command_timeout = 200;
-
-        # scott-desktop as scott in ~ on main at 3cae0f8d
-        # ^^^^^^^^^^^^^    ^^^^^    ^    ^^^^    ^^^^^^^^
-        format = lib.concatStrings [
-          "($hostname (\\($localip\\) )as )"
-          "($username in )"
-          "$directory"
-          "( \\($container$nix_shell\\))"
-          "( on $git_branch)( at $git_commit)( \\($git_status\\))( \\[$git_state\\])"
-          "( with $package)"
-          "( \\($nodejs$rust\\))"
-          "( took $cmd_duration)"
-          "$line_break"
-          "(\\($jobs\\) )$character"
-        ];
-
-        hostname = {
-          format = "[$hostname]($style)";
-          ssh_only = false;
-          style = "bold bright-purple";
-          trim_at = ".";
-        };
-
-        localip = {
-          disabled = false;
-          format = "[$localipv4]($style)";
-          ssh_only = false;
-          style = "bright-purple";
-        };
-
-        username = {
-          format = "[$user]($style)";
-          show_always = true;
-          style_user = "bold bright-blue";
-        };
-
-        container = {
-          format = "[$symbol $name]($style)";
-          style = "bright-cyan";
-          symbol = "container";
-        };
-
-        nix_shell = {
-          format = "[$symbol $name]($style)";
-          style = "bright-cyan";
-          symbol = "nix";
-        };
-
-        directory = {
-          format = "[$path]($style)[$read_only]($read_only_style)";
-          read_only = "*";
-          read_only_style = "bold bright-red";
-          repo_root_format = "[$before_root_path]($before_repo_root_style)[$repo_root]($repo_root_style)[$path]($style)[$read_only]($read_only_style)";
-          style = "bold bright-cyan";
-          truncation_symbol = "...";
-        };
-
-        git_branch = {
-          format = "[$branch(:$remote_branch)]($style)";
-          style = "bold bright-green";
-        };
-
-        git_commit = {
-          format = "[$hash(, $tag)]($style)";
-          style = "bold bright-green";
-          tag_disabled = false;
-          tag_symbol = "";
-        };
-
-        git_status = {
-          ahead = "⇡";
-          behind = "⇣";
-          conflicted = "=";
-          deleted = "-";
-          diverged = "⇕";
-          format = "[$all_status$ahead_behind]($style)";
-          modified = "!";
-          renamed = "^";
-          staged = "+";
-          stashed = "~";
-          style = "bright-green";
-          untracked = "?";
-          up_to_date = "";
-        };
-
-        git_state = {
-          am = "apply mailbox";
-          am_or_rebase = "apply mailbox/rebase";
-          bisect = "bisect";
-          cherry_pick = "cherry pick";
-          format = "[$state( $progress_current/$progress_total)]($style)";
-          merge = "merge";
-          rebase = "rebase";
-          revert = "revert";
-          style = "bright-green";
-        };
-
-        package = {
-          format = "[$version]($style)";
-          style = "bold bright-yellow";
-        };
-
-        nodejs = {
-          format = "[$symbol( $version)]($style)";
-          not_capable_style = "bright-red";
-          style = "bright-yellow";
-          symbol = "node";
-        };
-
-        rust = {
-          format = "[$symbol( $version)]($style)";
-          style = "bright-yellow";
-          symbol = "rust";
-        };
-
-        cmd_duration = {
-          format = "[$duration]($style)";
-          min_time = 1000;
-          style = "bright-red";
-        };
-
-        jobs = {
-          format = "[$number]($style)";
-          number_threshold = 1;
-          style = "bright-red";
-        };
-
-        character = {
-          error_symbol = "[❯](bold bright-red)";
-          success_symbol = "[❯](bold)";
-        };
-      };
+      defaultCacheTtl = 15 * 60;
+      defaultCacheTtlSsh = 15 * 60;
+      enableSshSupport = true;
+      maxCacheTtl = 60 * 60;
+      maxCacheTtlSsh = 60 * 60;
+      pinentryFlavor = "tty";
+      sshKeys = ["6C560B0B5F2146329B7AC36992F7186F2EE51AD4"];
     };
   };
 }
