c97c488e48e2768af331ea20319d9beabb92dd93
Scott Moore <me@scottwillmoore.au>
2023-05-25 12:49:23 +1000

    Install core packages

--- home.nix
+++ home.nix
@@ -3,10 +3,63 @@
   settings,
   ...
 }: {
+  # TODO: Consider allowUnfreePredicate instead.
+  nixpkgs.config.allowUnfree = true;
+
   home = {
     homeDirectory = "/home/${settings.userName}";
     packages = with packages; [
+      # TODO: Organize this list.
+      # TODO: Use Home Mananger modules (enable = true).
+
       alejandra
+
+      # https://wiki.archlinux.org/title/Core_utilities#Alternatives
+      # https://github.com/ibraheemdev/modern-unix
+      # https://news.ycombinator.com/item?id=26559334
+      bat
+      bottom
+      delta
+      du-dust
+      exa
+      fd
+      fzf
+      hyperfine
+      jq
+      ripgrep
+      tealdeer
+
+      # Core utilities
+      # https://wiki.archlinux.org/title/Core_utilities
+      # https://wiki.archlinux.org/title/Archiving_and_compression
+      # https://wiki.archlinux.org/title/Network_tools
+      bzip2
+      coreutils
+      curl
+      curl
+      diffutils
+      dig
+      file
+      findutils
+      gawk
+      gnugrep
+      gnused
+      gnutar
+      gzip
+      iputils
+      less
+      netcat
+      # TODO: Enable allowUnfree.
+      # rar
+      time
+      traceroute
+      # TODO: Enable allowUnfree.
+      # unrar
+      unzip
+      wget
+      wget
+      xz
+      zip
     ];
     stateVersion = "22.11";
     username = settings.userName;
@@ -15,7 +68,15 @@
   programs = {
     bash.enable = true;
 
+    command-not-found.enable = true;
+
     dircolors.enable = true;
+
+    direnv = {
+      enable = true;
+
+      nix-direnv.enable = true;
+    };
 
     git = {
       enable = true;
