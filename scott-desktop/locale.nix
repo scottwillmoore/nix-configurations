f5b6a7085c4c1f5763b1e06f9f7d5726493935c6
Scott Moore <me@scottwillmoore.au>
2024-12-27 14:53:30 +1100

    Fix the location service with the Google Geolocation API

--- scott-desktop/locale.nix
+++ scott-desktop/locale.nix
@@ -1,4 +1,6 @@
 {
+  # Language
+
   i18n.defaultLocale = "en_AU.UTF-8";
 
   i18n.extraLocaleSettings = {
@@ -12,7 +14,22 @@
     "en_US.UTF-8/UTF-8"
   ];
 
-  time.timeZone = "Australia/Melbourne";
+  # Location
 
   location.provider = "geoclue2";
+
+  # https://github.com/NixOS/nixpkgs/issues/321121
+  # https://gitlab.archlinux.org/archlinux/packaging/packages/geoclue
+  # The Mozilla Location Service (MLS) has been shutdown. Therefore, until a
+  # replacement has been found use the Google Geolocation API and borrow the
+  # Arch Linux API key... Sorry!
+  services.geoclue2.geoProviderUrl =
+    let
+      apiKey = "AIzaSyDwr302FpOSkGRpLlUpPThNTDPbXcIn_FM";
+    in
+    "https://www.googleapis.com/geolocation/v1/geolocate?key=${apiKey}";
+
+  # Time
+
+  time.timeZone = "Australia/Melbourne";
 }
