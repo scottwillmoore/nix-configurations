39c74341713ac79404553db253b42adcb5b19157
Scott Moore <me@scottwillmoore.au>
2025-04-20 14:21:04 +1000

    Refactor Kitty and Ghostty configurations

--- modules/home/scott/kitty.nix
+++ modules/home/scott/kitty.nix
@@ -1,50 +1,59 @@
+{ lib, ... }:
 {
   programs.kitty = {
     enable = true;
 
-    font.name = "Iosevka";
-    font.size = 16;
-
     settings = {
+      # Font
+      font_family = "Iosevka";
+      font_size = 16;
+      symbol_map =
+        let
+          codePoints = lib.concatStringsSep "," [
+            "U+23FB-U+23FE"
+            "U+2665"
+            "U+26A1"
+            "U+2B58"
+            "U+E000-U+E00A"
+            "U+E0A0-U+E0A2"
+            "U+E0A3"
+            "U+E0B0-U+E0B3"
+            "U+E0B4-U+E0C8"
+            "U+E0CA"
+            "U+E0CC-U+E0D7"
+            "U+E200-U+E2A9"
+            "U+E300-U+E3E3"
+            "U+E5FA-U+E6B7"
+            "U+E700-U+E8EF"
+            "U+EA60-U+EC1E"
+            "U+ED00-U+EFCE"
+            "U+F000-U+F2FF"
+            "U+F300-U+F381"
+            "U+F400-U+F533"
+            "U+F500-U+FD46"
+            "U+F0001-U+F1AF0"
+          ];
+        in
+        "${codePoints} Symbols Nerd Font";
+
+      # Scrollback
       scrollback_lines = 1024;
       scrollback_pager_history_size = 128;
 
+      # Terminal bell
       enable_audio_bell = false;
 
+      # Mouse
       mouse_hide_wait = 1;
 
+      # Window layout
       hide_window_decorations = true;
 
+      # Tab bar
       tab_bar_style = "powerline";
       tab_bar_min_tabs = 1;
     };
 
     themeFile = "tokyo_night_night";
-
-    extraConfig = ''
-      # https://github.com/ryanoasis/nerd-fonts/wiki/Glyph-Sets-and-Code-Points (v3.3.0)
-      symbol_map U+23FB-U+23FE Symbols Nerd Font Mono
-      symbol_map U+2665 Symbols Nerd Font Mono
-      symbol_map U+26A1 Symbols Nerd Font Mono
-      symbol_map U+2B58 Symbols Nerd Font Mono
-      symbol_map U+E000-U+E00A Symbols Nerd Font Mono
-      symbol_map U+E0A0-U+E0A2 Symbols Nerd Font Mono
-      symbol_map U+E0A3 Symbols Nerd Font Mono
-      symbol_map U+E0B0-U+E0B3 Symbols Nerd Font Mono
-      symbol_map U+E0B4-U+E0C8 Symbols Nerd Font Mono
-      symbol_map U+E0CA Symbols Nerd Font Mono
-      symbol_map U+E0CC-U+E0D7 Symbols Nerd Font Mono
-      symbol_map U+E200-U+E2A9 Symbols Nerd Font Mono
-      symbol_map U+E300-U+E3E3 Symbols Nerd Font Mono
-      symbol_map U+E5FA-U+E6B7 Symbols Nerd Font Mono
-      symbol_map U+E700-U+E8EF Symbols Nerd Font Mono
-      symbol_map U+EA60-U+EC1E Symbols Nerd Font Mono
-      symbol_map U+ED00-U+EFCE Symbols Nerd Font Mono
-      symbol_map U+F000-U+F2FF Symbols Nerd Font Mono
-      symbol_map U+F300-U+F381 Symbols Nerd Font Mono
-      symbol_map U+F400-U+F533 Symbols Nerd Font Mono
-      symbol_map U+F500-U+FD46 Symbols Nerd Font Mono
-      symbol_map U+F0001-U+F1AF0 Symbols Nerd Font Mono
-    '';
   };
 }
