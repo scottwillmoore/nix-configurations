59af2a5e25cdc6e007229d83496d79e1325ef66e
Scott Moore <me@scottwillmoore.au>
2026-07-22 20:47:06 +1000

    Remove old utilities from flake modules

--- modules/flake/imports.nix
+++ modules/flake/imports.nix
@@ -38,7 +38,6 @@ let
   commonArgs = {
     inherit inputs;
     inherit outputs;
-    # inherit settings;
   };
 in
 {
--- modules/flake/outputs.nix
+++ modules/flake/outputs.nix
@@ -7,7 +7,6 @@
 }:
 {
   imports = [
-    # https://github.com/hercules-ci/flake-parts/issues/67
     (lib.mkAliasOptionModule [ "outputs" ] [ "flake" ])
 
     {
