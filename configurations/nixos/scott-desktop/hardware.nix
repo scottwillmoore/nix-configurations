1cb2f5ab889fc9bb6711634e10ce694ef01af7d1
Scott Moore <me@scottwillmoore.au>
2026-07-26 22:01:09 +1000

    scott-desktop: Set eno1 as a trusted interface

--- configurations/nixos/scott-desktop/hardware.nix
+++ configurations/nixos/scott-desktop/hardware.nix
@@ -9,6 +9,12 @@
   hardware.cpu.intel.updateMicrocode = true;
   # TODO: https://github.com/NixOS/nixos-hardware/blob/master/common/gpu/intel/coffee-lake/default.nix
 
+  # Ethernet
+
+  networking.firewall.trustedInterfaces = [
+    "eno1"
+  ];
+
   # GPU
 
   # boot.initrd.kernelModules = [
