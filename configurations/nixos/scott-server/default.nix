59a3061a27bd7a8984246a05656d3d68e83cd7aa
Scott Moore <me@scottwillmoore.au>
2026-07-26 22:03:28 +1000

    scott-server: Add authorized keys for OpenSSH

--- configurations/nixos/scott-server/default.nix
+++ configurations/nixos/scott-server/default.nix
@@ -12,4 +12,8 @@
     hostName = "scott-server";
     hostPlatform = "x86_64-linux";
   };
+
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHpwam0hQH7uJUC6DfeApxxHQ07t47SzSVT/kHmOgDkT"
+  ];
 }
